91009608605da8b67cc7adb441cb6fb26dcf3e1a
David Papamichael <davidpapamichael@gmail.com>
2019-12-16 16:27:00 +0000

    Initialize method added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,9 @@
 class Oystercard
 
+    attr_reader :money
+    
+    def initialize
+      @money = 0
+    end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require "oystercard"
 
 describe Oystercard do
 
+  describe "#initialize" do
+    it "has a balance of 0 by default" do
+      expect(subject.money).to eq(0)
+    end
+  end
+
 end
