7637b0a1f33217886aaf54579ac886c6272f6df8
David Papamichael <davidpapamichael@gmail.com>
2019-12-17 11:29:46 +0000

    Pay method added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,20 @@
 class Oystercard
 
     attr_accessor :balance
+
+    MAX_CAP = 90
     
     def initialize
       @balance = 0
     end
 
     def top_up(money)
+      fail "Maximum limit of #{MAX_CAP} reached" if (@balance + money) > MAX_CAP
       @balance += money
     end
+
+    def pay(fare)
+      @balance -= fare
+    end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,15 +9,27 @@ describe Oystercard do
   end
 
   describe "#topup" do
-
-    it { is_expected .to respond_to(:top_up).with(1).argument }
     
-    it "adds 5 money to the balance" do
-      card = Oystercard.new
-      card.top_up(5)
-      expect(card.balance).to eq(5)
+    context "add money" do
+      it { is_expected .to respond_to(:top_up).with(1).argument }
+    
+      it "adds 5 money to the balance" do
+        subject.top_up(5)
+        expect(subject.balance).to eq(5)
+      # it {expect{ subject.top_up 1 }.to change{ subject.balance }.by 1}
+      end
 
-    # expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      it "has a limit of 90" do
+        max_cap = Oystercard::MAX_CAP
+        subject.top_up(max_cap)
+        expect { subject.top_up(1) }.to raise_error "Maximum limit of #{max_cap} reached"
+      end
     end
   end
+
+  describe "#pay" do
+    it { is_expected .to respond_to(:pay).with(1).argument }
+    it { expect{ subject.pay(1) }.to change{ subject.balance }.by -1 }
+  end
+
 end
